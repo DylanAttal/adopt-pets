4840b70599eab4293e956d8dffc308055336723e
Dylan Attal <attal.dylan@gmail.com>
2019-04-18 10:16:12 -0400

    Adds pet form and toggle capability

--- src/views/Pet.vue
+++ src/views/Pet.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>Pet</div>
+  <div>
+    <h1>{{ animal.name }}</h1>
+    <h2>Age: {{ animal.age }} years old</h2>
+    <h3>Breed: {{ animal.breed }}</h3>
+  </div>
 </template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      animal: {}
+    };
+  },
+  computed: {
+    ...mapState(["cats", "dogs"])
+  },
+  mounted() {
+    const animal = this[this.$route.params.species][this.$route.params.id];
+    this.animal = animal;
+  }
+};
+</script>
